b668b3914100a289faa472029c93b7432f29377c
Cristian <cristiankhan23@gmail.com>
2024-08-29 18:25:07 -0300

    Get task and delete task

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from .database import get_all_tasks, create_task, get_task_by_title
+from .database import get_all_tasks, create_task, delete_task, get_task_by_title, get_task_by_id
 from .models import Task
 
 app = FastAPI()
@@ -25,13 +25,20 @@ async def save_task(task: Task):
     raise HTTPException(status_code=400, detail='Error to create the document')
 
 @app.get('/api/tasks/{id}')
-async def get_task():
-    return 'single task'
+async def get_task(id: str):
+    task_found  = await get_task_by_id(id)
+    if task_found:
+        task_found['_id'] = str(task_found['_id'])
+        return task_found
+    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
 
 @app.put('/api/tasks/{id}')
 async def update_tasks():
     return 'updating task'
 
 @app.delete('/api/tasks/{id}')
-async def delete_task():
-    return 'delete task'
+async def remove_task(id: str):
+    response = await delete_task(id)
+    if response:
+        return 'Successfully deleted'
+    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')

4d891c970738257a1492e3455c2f024d953ceb7e
Cristian <cristiankhan23@gmail.com>
2024-08-30 11:17:00 -0300

    Extract routes of main to task file

--- server/main.py
+++ server/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from .database import get_all_tasks, create_task, delete_task, update_task, get_task_by_title, get_task_by_id
-from .models import Task, UpdateTask
+from fastapi import FastAPI
+from .routes.task import task
 
 app = FastAPI()
 
@@ -8,41 +7,4 @@ app = FastAPI()
 def root():
     return {'Wellcome': 'Wellcome to the API'}
 
-@app.get('/api/tasks')
-async def get_tasks():
-    tasks = await get_all_tasks()
-    return tasks
-
-@app.post('/api/tasks')
-async def save_task(task: Task):
-    task_found = await get_task_by_title(task.title)
-    if task_found:
-        raise HTTPException(status_code=409, detail='Task already exists')
-
-    response = await create_task(task.dict())
-    if response:
-        return response
-    raise HTTPException(status_code=400, detail='Error to create the document')
-
-@app.get('/api/tasks/{id}')
-async def get_task(id: str):
-    task_found = await get_task_by_id(id)
-    if task_found:
-        task_found['_id'] = str(task_found['_id'])
-        return task_found
-    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
-
-@app.put('/api/tasks/{id}')
-async def modify_task(id: str, task: UpdateTask):
-    response = await update_task(id, task)
-    if response:
-        response['_id'] = str(response['_id'])
-        return response
-    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
-
-@app.delete('/api/tasks/{id}')
-async def remove_task(id: str):
-    response = await delete_task(id)
-    if response:
-        return 'Successfully deleted'
-    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
+app.include_router(task)
---
+++ server/routes/task.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, HTTPException
+from ..database import get_all_tasks, create_task, delete_task, update_task, get_task_by_title, get_task_by_id
+from ..models import Task, UpdateTask
+
+task = APIRouter()
+
+@task.get('/api/tasks')
+async def get_tasks():
+    tasks = await get_all_tasks()
+    return tasks
+
+@task.post('/api/tasks')
+async def save_task(task: Task):
+    task_found = await get_task_by_title(task.title)
+    if task_found:
+        raise HTTPException(status_code=409, detail='Task already exists')
+
+    response = await create_task(task.dict())
+    if response:
+        return response
+    raise HTTPException(status_code=400, detail='Error to create the document')
+
+@task.get('/api/tasks/{id}')
+async def get_task(id: str):
+    task_found = await get_task_by_id(id)
+    if task_found:
+        task_found['_id'] = str(task_found['_id'])
+        return task_found
+    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
+
+@task.put('/api/tasks/{id}')
+async def modify_task(id: str, task: UpdateTask):
+    response = await update_task(id, task)
+    if response:
+        response['_id'] = str(response['_id'])
+        return response
+    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')
+
+@task.delete('/api/tasks/{id}')
+async def remove_task(id: str):
+    response = await delete_task(id)
+    if response:
+        return 'Successfully deleted'
+    raise HTTPException(status_code=404, detail=f'Task not found by id {id}')

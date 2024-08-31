0325ee333ce26cb7d4b4b3e4d3c5fe48c1cbc8c9
Cristian <cristiankhan23@gmail.com>
2024-08-31 14:20:44 -0300

    CORS settings. Add environment var.

--- server/main.py
+++ server/main.py
@@ -1,7 +1,23 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from .routes.task import task
 
+load_dotenv()
+CLIENT_URL = os.getenv("CLIENT_URL")
+
 app = FastAPI()
+
+#CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[CLIENT_URL],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get('/')
 def root():

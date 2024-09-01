a52fe88b1243824b33f260444db9a37235a6dbfe
Cristian <cristiankhan23@gmail.com>
2024-09-01 11:12:47 -0300

    Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,23 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import TaskList from '../components/TaskList'
+
 function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const response = await axios.get('http://localhost:8000/api/tasks')
+      setTasks(response.data)
+    }
+    fetchTasks()
+  }, [])
+
   return (
-    <h1 className="text-3xl font-bold">Home</h1>
+    <>
+      <h1 className="text-3xl font-bold">Home</h1>
+      <TaskList tasks={tasks}/>
+    </>
   )
 }
 

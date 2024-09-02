dc9a1e117e936dfa9d09abb28a02374e356072b6
Cristian <cristiankhan23@gmail.com>
2024-09-02 12:15:29 -0300

    Invoke TaskCard component

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,12 +1,11 @@
+import TaskCard from '../components/TaskCard'
+
 function TaskList({tasks}) {
   return (
     <div className="grid grid-cols-3 gap-4">
       {
-        tasks.map((task, index) => (
-          <div key={index} className="bg-zinc-950 p-4 hover:cursor-pointer hover:bg-gay-950">
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
-          </div>
+        tasks.map((task) => (
+          <TaskCard task={task} key={task._id} />
         ))
       }
     </div>

e2d7575e2e953437a201727d6c8d7d9be478e4ec
Cristian <cristiankhan23@gmail.com>
2024-09-03 09:43:32 -0300

    Logic for delete task. Some styles

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -43,24 +43,45 @@ function TaskForm() {
 
   return (
     <div className="flex items-center justify-center h-[calc(100vh-10rem)]">
-      <form className="bg-zinc-950 p-10" onSubmit={handleSubmit}>
-        <input 
-          className="block py-2 px-3 mb-4 w-full text-black"
-          type="text"
-          placeholder="Title"
-          autoFocus
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}/>
-        <textarea 
-          className="block py-2 px-3 mb-4 w-full text-black"
-          rows="3"
-          placeholder="Description"
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}></textarea>
-        <button>
+      <div>
+        <form className="bg-zinc-950 p-10" onSubmit={handleSubmit}>
+          <h1 className="text-3xl font-bold my-4">
           {params.id ? "Update task" : "Create task"}
-        </button>
-      </form>
+          </h1>
+          <input 
+            className="block py-2 px-3 mb-4 w-full text-black"
+            type="text"
+            placeholder="Title"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}/>
+          <textarea 
+            className="block py-2 px-3 mb-4 w-full text-black"
+            rows="3"
+            placeholder="Description"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}></textarea>
+          <button className="bg-white hover:bg-slate-800 hover:text-white text-slate-800 py-2 px-4 rounded">
+            {params.id ? "Update" : "Create"}
+          </button>
+        </form>
+        {
+          params.id && (
+            <button 
+              className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded mt-5"
+              onClick={async () => {
+                try {
+                  const response = await axios.delete(`http://localhost:8000/api/tasks/${params.id}`)
+                  nav('/')
+                } catch (error) {
+                  console.log(error)
+                }
+              }}>
+              Delete
+            </button>
+          )
+        }
+      </div>
     </div>
   )
 }

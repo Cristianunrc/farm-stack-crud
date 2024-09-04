3616ab12e32e6b5fafff6b6cb2bc7b511cd43b05
Cristian <cristiankhan23@gmail.com>
2024-09-04 09:21:24 -0300

    Change title for icon home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,18 @@ function Navbar() {
   return (
     <header className="flex justify-between items-center my-7">
       <Link to="/">
-        <h1 className="text-3xl font-bold">Task App</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
       </Link>
       <Link to="/tasks/new"
             className="bg-zinc-950 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded">

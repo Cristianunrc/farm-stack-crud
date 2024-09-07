14670e7d020c9d9cb93a0ac0a845524d4b93f605
Cristian <cristiankhan23@gmail.com>
2024-09-06 22:58:25 -0300

    Up the list of cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <BrowserRouter>
-      <div className="container mx-auto px-10">
+      <div className="container mx-auto px-10 pb-10">
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>

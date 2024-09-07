46e6de4926eebb13d60582290b1bd0c8be722afb
Cristian <cristiankhan23@gmail.com>
2024-09-07 09:35:22 -0300

    TaskCard styles

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -6,35 +6,36 @@ function TaskCard({task}) {
 
   return (
     <div
-      className="bg-zinc-950 p-4 hover:cursor-pointer hover:bg-gay-950"
+      className="relative bg-zinc-950 p-4 hover:cursor-pointer hover:bg-gay-950"
       onClick={() => { nav(`/tasks/${task._id}`)}}>
-      <div className="flex justify-between">
-        <div className="w-full border-b-2 border-slate-600 pb-2 mb-2">
-          <h2 className="font-bold text-2xl text-center">{task.title}</h2>
+      <button
+        className="absolute top-1 right-1"
+        onClick={async (e) => {
+          e.stopPropagation()
+          const response = await updateTask(task._id, {completed: !task.completed})
+          if (response.status === 200) {
+            window.location.reload()
+          }
+        }}>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={`w-6 h-6 ${task.completed? 'text-green-500' : ''}`}>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 12.75 6 6 9-13.5" />
+        </svg>
+      </button>
+      <div className="flex flex-col items-center">
+        <div className="w-full border-b-2 border-slate-600 pb-2 mb-2 mt-4">
+          <h2 className="font-bold text-2xl text-center break-words">{task.title}</h2>
         </div>
-        <button
-          onClick={async (e) => {
-            e.stopPropagation()
-            const response = await updateTask(task._id, {completed: !task.completed})
-            if (response.status === 200) {
-              window.location.reload()
-            }
-          }}>
-          <svg 
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className={`size-6 ${task.completed? 'text-green-500' : ''}`}>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m4.5 12.75 6 6 9-13.5" />
-          </svg>
-        </button>
       </div>
-      <p className="text-slate-300">{task.description}</p>
+      <p className="text-slate-300 break-words">{task.description}</p>
     </div>
   )
 }

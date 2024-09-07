aa3e8f762b92283862a66bacc7ab641944159bda
Cristian <cristiankhan23@gmail.com>
2024-09-06 23:01:31 -0300

    Adjust buttons

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -37,9 +37,9 @@ function TaskForm() {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-10rem)]">
       <div>
-        <form className="bg-zinc-950 p-10" onSubmit={handleSubmit}>
-          <h1 className="text-3xl font-bold my-4">
-          {params.id ? "Update task" : "Create task"}
+        <form className="bg-zinc-950 p-5" onSubmit={handleSubmit}>
+          <h1 className="text-3xl text-center font-bold my-4 ">
+            {params.id ? "Update task" : "Create task"}
           </h1>
           <input 
             className="block py-2 px-3 mb-4 w-full text-black"
@@ -47,36 +47,35 @@ function TaskForm() {
             placeholder="Title"
             autoFocus
             onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            required/>
+            value={title} required/>
           <textarea 
             className="block py-2 px-3 mb-4 w-full text-black"
             rows="3"
             placeholder="Description"
             onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            required></textarea>
-          <button 
-            className="bg-white hover:bg-slate-800 hover:text-white text-slate-800 py-2 px-4 rounded">
-            {params.id ? "Update" : "Create"}
-          </button>
-        </form>
-        {
-          params.id && (
+            value={description} required></textarea>
+          <div className="flex justify-center space-x-4">
             <button 
-              className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded mt-5"
-              onClick={async () => {
-                try {
-                  const response = await deleteTask(params.id)
-                  nav('/')
-                } catch (error) {
-                  console.log(error)
-                }
-              }}>
-              Delete
+              className="bg-white hover:bg-slate-800 hover:text-white text-slate-800 py-2 px-4 rounded">
+              {params.id ? "Update" : "Create"}
             </button>
-          )
-        }
+            {params.id && (
+                <button 
+                  className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded"
+                  onClick={async () => {
+                    try {
+                      const response = await deleteTask(params.id)
+                      nav('/')
+                    } catch (error) {
+                      console.log(error)
+                    }
+                  }}>
+                  Delete
+                </button>
+              )
+            }
+          </div>
+        </form>
       </div>
     </div>
   )

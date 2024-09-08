6ac701cd2e668affdd70b643a20ff4e28dc8553c
Cristian <cristiankhan23@gmail.com>
2024-09-08 12:52:38 -0300

    Change hover button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
         </svg>
       </Link>
       <Link to="/tasks/new"
-            className="bg-zinc-950 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded">
+            className="bg-zinc-950 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded">
         Create Task
       </Link>
     </header>
